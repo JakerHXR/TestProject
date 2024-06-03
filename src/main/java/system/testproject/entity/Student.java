b7998c9721e39535adb4ad9eb38c42a5d9dc8e79
JakerHXR <jarkerh@gmail.com>
2024-06-03 17:07:36 +0800

    增加学生实体

---
+++ src/main/java/system/testproject/entity/Student.java
@@ -0,0 +1,64 @@
+package system.testproject.entity;
+
+/**
+ * 学生表
+ * @author Jaker
+ */
+public class Student {
+    private String stuId;
+    private String stuName;
+    private String stuPhone;
+    private String stuAddress;
+
+    public Student() {
+    }
+
+    public Student(String stuId, String stuName, String stuPhone, String stuAddress) {
+        this.stuId = stuId;
+        this.stuName = stuName;
+        this.stuPhone = stuPhone;
+        this.stuAddress = stuAddress;
+    }
+
+    public String getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(String stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getStuPhone() {
+        return stuPhone;
+    }
+
+    public void setStuPhone(String stuPhone) {
+        this.stuPhone = stuPhone;
+    }
+
+    public String getStuAddress() {
+        return stuAddress;
+    }
+
+    public void setStuAddress(String stuAddress) {
+        this.stuAddress = stuAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuId='" + stuId + '\'' +
+                ", stuName='" + stuName + '\'' +
+                ", stuPhone='" + stuPhone + '\'' +
+                ", stuAddress='" + stuAddress + '\'' +
+                '}';
+    }
+}
