7a4d361283280f04acd9de9ea00596f99d39047e
JakerHXR <jarkerh@gmail.com>
2024-06-08 17:07:17 +0800

    增加mybatis-plus的mapper扫描

--- src/main/java/system/testproject/TestProjectApplication.java
+++ src/main/java/system/testproject/TestProjectApplication.java
@@ -1,9 +1,11 @@
 package system.testproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("system.testproject.mapper")
 public class TestProjectApplication {
 
     public static void main(String[] args) {
