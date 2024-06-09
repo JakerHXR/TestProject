67545b2535697fdb5a5feff77c1a8d461fe756b9
JakerHXR <jarkerh@gmail.com>
2024-06-09 22:48:15 +0800

    完善题目mapper文件

--- src/main/java/system/testproject/mapper/QuestionMapper.java
+++ src/main/java/system/testproject/mapper/QuestionMapper.java
@@ -1,12 +1,28 @@
 package system.testproject.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import system.testproject.entity.Questions;
 
+import java.util.List;
+
+/**
+ *
+ * @author Jaker
+ */
+@Mapper
 public interface QuestionMapper extends BaseMapper<Questions> {
     //add a new question
     int addQuestion(Questions questions);
 
     //update the question
     int  updateQuestion(Questions questions);
+
+    //output 20 questions randomly
+    List<Questions> getRandomQuestions();
+
+    //get all questions
+    @Select("select * from questions")
+    List<Questions> getAll();
 }
