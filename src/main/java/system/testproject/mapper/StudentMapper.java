bbfc133bdf7680764ce83362f7a037bc8d009bb9
JakerHXR <jarkerh@gmail.com>
2024-06-17 22:58:41 +0800

    修正学生删除sql

--- src/main/java/system/testproject/mapper/StudentMapper.java
+++ src/main/java/system/testproject/mapper/StudentMapper.java
@@ -1,9 +1,18 @@
 package system.testproject.mapper;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import system.testproject.entity.Student;
 import system.testproject.entity.User;
 
 public interface StudentMapper {
     @Select("SELECT * FROM student WHERE userid = #{userid}")
-    User findByUserId(String userid);
+    Student findByUserId(String userid);
+
+    int addStudent(Student student);
+
+    int updateStudent(Student student);
+
+    int removeStudent(String userId);
 }
